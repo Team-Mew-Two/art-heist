43b625fb890eba862f5ddb836a89dbfc43d3353a
Josh Naso <joshnaso@Joshs-MacBook-Pro.local>
2020-03-11 10:37:05 -0400

    can get items with fetch call

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -7,5 +7,9 @@ router.get('/getArt', apiController.getArt, apiController.insertArt, (req, res) 
   res.status(200);
 })
 
+router.get('/getItems', apiController.getItems, (req, res) => {
+  res.status(200).json(res.locals.items);
+})
+
 
 module.exports = router;
